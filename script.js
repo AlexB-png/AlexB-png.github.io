33664027a0ba27bd3d99d09f8028254399f0d6e2
goldensyrup-png <alexander.brankin.466@accesscreative.ac.uk>
2025-06-20 00:33:19 +0100

    start

---
+++ script.js
@@ -0,0 +1,52 @@
+document.getElementById("layer1-1").classList.toggle("show")
+document.getElementById("layer1-2").classList.toggle("show")
+document.getElementById("layer2-1").classList.toggle("show")
+document.getElementById("layer2-2").classList.toggle("show")
+document.getElementById("aboutMe").classList.toggle("show")
+document.getElementById("contacts").classList.toggle("show")
+
+function layer1() {
+    document.getElementById("layer2-1").classList.remove("show")
+    document.getElementById("layer2-2").classList.remove("show")
+    document.getElementById("layer3-projects").classList.remove("show")
+    document.getElementById("layer3-pictures").classList.remove("show")
+    document.getElementById("aboutMe").classList.remove("show")
+    document.getElementById("contacts").classList.remove("show")
+    document.getElementById("aboutMeList").classList.remove("show")
+    document.getElementById("contactsList").classList.remove("show")
+    
+    document.getElementById("layer1-1").classList.toggle("show")
+    document.getElementById("layer1-2").classList.toggle("show")
+}
+
+function layer21() {
+    document.getElementById("layer2-1").classList.toggle("show")
+    document.getElementById("layer2-2").classList.toggle("show")
+
+    document.getElementById("layer2-1").classList.remove("show")
+    document.getElementById("layer2-2").classList.remove("show")
+}
+
+function showProjects() {
+    document.getElementById("layer3-projects").classList.toggle("show")
+}
+
+function showPictures() {
+    document.getElementById("layer3-pictures").classList.toggle("show")
+}
+
+function layer22() {
+    document.getElementById("aboutMe").classList.toggle("show")
+    document.getElementById("contacts").classList.toggle("show")
+
+    document.getElementById("contactsList").classList.remove("show")
+    document.getElementById("aboutMeList").classList.remove("show")
+}
+
+function contactShow() {
+    document.getElementById("contactsList").classList.toggle("show")
+}
+
+function aboutShow() {
+    document.getElementById("aboutMeList").classList.toggle("show")
+}
